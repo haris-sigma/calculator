ebd3a133f705ed115b79ab9e1d3237067eee4616
sigmaschooltech <126048372+haris-sigma@users.noreply.github.com>
2023-03-20 06:40:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function add() {
+  const firstNumber = parseFloat(prompt("Enter the first number:"));
+  const secondNumber = parseFloat(prompt("Enter the second number:"));
+  const result = firstNumber + secondNumber;
+  alert("The result is: " + result);
+}
